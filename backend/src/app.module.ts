8559e60b4efd71c44a2d48e9801f468b25879ff9
lecong12 <87135104+lecong12@users.noreply.github.com>
2026-03-01 11:14:27 +0700

    Cập nhật

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,7 +22,7 @@ import { AuthModule } from './modules/auth/auth.module';
         MongooseModule.forRootAsync({
             imports: [ConfigModule],
             useFactory: async (configService: ConfigService) => ({
-                uri: configService.get<string>('MONGODB_URI'),
+                uri: configService.get<string>('MONGO_URI'),
             }),
             inject: [ConfigService],
         }),
