8ede423c8b6fe067ec2df89efc275014fc6569fe
lecong12Home <lecong12@gmail.com>
2026-03-01 16:52:46 +0700

    Cập nhật

--- backend/src/vercel.ts
+++ backend/src/vercel.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
+import express from 'express';
 
 const server = express();
 
