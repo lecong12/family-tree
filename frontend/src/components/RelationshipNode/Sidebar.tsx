e72729fa4cd1e6e9a686ad46e19b985673d93e52
lecong12Home <lecong12@gmail.com>
2026-03-04 17:59:57 +0700

    Cập nhật

--- frontend/src/components/RelationshipNode/Sidebar.tsx
+++ frontend/src/components/RelationshipNode/Sidebar.tsx
@@ -16,39 +16,38 @@ const AdminSidebar = () => {
   return (
     <div className="h-screen border-r border-gray-200">
       <Sidebar aria-label="Admin sidebar">
-        <Sidebar.Items>
-          <Sidebar.ItemGroup>
-            {/* Mục Tổng quan */}
-            <Sidebar.Item href="/admin" icon={HiChartPie}>
-              Dashboard
-            </Sidebar.Item>
+        {/* The <Sidebar.Items> wrapper is removed to fix a potential version conflict with flowbite-react */}
+        <Sidebar.ItemGroup>
+          {/* Mục Tổng quan */}
+          <Sidebar.Item href="/admin" icon={HiChartPie}>
+            Dashboard
+          </Sidebar.Item>
 
-            {/* Mục Quản lý thành viên */}
-            <Sidebar.Item href="/admin/persons" icon={HiUser}>
-              Quản lý thành viên
-            </Sidebar.Item>
+          {/* Mục Quản lý thành viên */}
+          <Sidebar.Item href="/admin/persons" icon={HiUser}>
+            Quản lý thành viên
+          </Sidebar.Item>
 
-            {/* NÚT IMPORT CSV - CHÚNG TA THÊM Ở ĐÂY */}
-            <Sidebar.Item 
-              href="/admin/import"
-              icon={HiUpload}
-              className="bg-blue-50 text-blue-700 hover:bg-blue-100"
-            >
-              Nạp dữ liệu (CSV)
-            </Sidebar.Item>
-          </Sidebar.ItemGroup>
+          {/* NÚT IMPORT CSV - CHÚNG TA THÊM Ở ĐÂY */}
+          <Sidebar.Item 
+            href="/admin/import"
+            icon={HiUpload}
+            className="bg-blue-50 text-blue-700 hover:bg-blue-100"
+          >
+            Nạp dữ liệu (CSV)
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
 
-          <Sidebar.ItemGroup>
-            {/* Mục Đăng xuất */}
-            <Sidebar.Item 
-              onClick={logout} 
-              icon={HiLogout} 
-              className="cursor-pointer text-red-600 hover:bg-red-50"
-            >
-              Đăng xuất
-            </Sidebar.Item>
-          </Sidebar.ItemGroup>
-        </Sidebar.Items>
+        <Sidebar.ItemGroup>
+          {/* Mục Đăng xuất */}
+          <Sidebar.Item 
+            onClick={logout} 
+            icon={HiLogout} 
+            className="cursor-pointer text-red-600 hover:bg-red-50"
+          >
+            Đăng xuất
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
       </Sidebar>
     </div>
   );
