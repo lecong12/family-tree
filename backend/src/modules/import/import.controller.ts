14a1413f3f6f7f86c4191c2bc80a949498f7f200
lecong12 <87135104+lecong12@users.noreply.github.com>
2026-03-04 14:45:26 +0700

    Cập nhật

--- backend/src/modules/import/import.controller.ts
+++ backend/src/modules/import/import.controller.ts
@@ -1,24 +1,30 @@
 import { 
   Controller, 
   Post, 
-  UseInterceptors,
-  UploadedFile,
+  UploadedFile, 
+  UseGuards, 
   BadRequestException,
+  UseInterceptors
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express'
 import { DataImportService } from './import.service';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';     // Nếu bạn có dùng phân quyền Role
+import { UserRoles } from '../../constants';
 
 @Controller('admin/import')
-// Tạm thời bỏ UseGuards để không phải import file đang bị lỗi path
+@UseGuards(JwtAuthGuard, RolesGuard) // Bảo vệ API bằng JWT và kiểm tra quyền
 export class DataImportController {
   constructor(private readonly dataImportService: DataImportService) {}
 
   @Post('csv')
+  @Roles(UserRoles.ADMIN)
   @UseInterceptors(FileInterceptor('file')) // 'file' phải khớp với tên field trong FormData khi upload
   async uploadCsv(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('Vui lòng chọn file CSV để nạp dữ liệu.');
     }
+
     // Kiểm tra định dạng file (chỉ chấp nhận csv)
     if (!file.originalname.match(/\.(csv)$/)) {
       throw new BadRequestException('Chỉ chấp nhận tệp định dạng .csv');
