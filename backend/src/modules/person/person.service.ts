bfe10f2b9ac5193afe5b34143ebc8bb01f537388
lecong12 <87135104+lecong12@users.noreply.github.com>
2026-03-06 14:57:05 +0700

    Cập nhật

--- backend/src/modules/person/person.service.ts
+++ backend/src/modules/person/person.service.ts
@@ -246,7 +246,7 @@ export class PersonService {
         // Nếu name rỗng, filter là {}, trả về 10 người đầu tiên
         const persons = await this.personModel
             .find(filter)
-            .select('_id name cccd slug') // Chỉ lấy các trường cần thiết
+            .select('_id name cccd slug avatar') // Chỉ lấy các trường cần thiết
             .limit(10) // Giới hạn 10 kết quả
             .exec();
         return persons;
